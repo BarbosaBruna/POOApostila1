5608a26c6ce3653795ef3cba8d1c7951e7c10e4a
Bruna Barbosa dos Santos <brunabarbosa_snts@hotmail.com>
2022-10-23 17:42:46 -0300

    POO(Apostila1) Ex2.4

--- TesteCarro.java
+++ TesteCarro.java
@@ -6,13 +6,17 @@ public class TesteCarro {
         Carro1.setCor("Vermelho");
         Carro1.viraEsquerda();
         Carro1.acelera();
- 
+        System.out.println(Carro1.getCor(null));
+        System.out.println(Carro1.getModelo(null));
+         
         
          Carro Carro2 = new Carro();
          Carro2.setModelo("Duster");
          Carro2.setCor("Prata");
          Carro2.viraEsquerda();
          Carro2.acelera();
+         System.out.println(Carro2.getModelo(null));
+         System.out.println(Carro2.getModelo(null));
 
 
     }
