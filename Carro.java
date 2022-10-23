19196e63900cee56bf7dfa005935367129576459
Bruna Aparecida <Bruna@MACECOC02V57EVH.local>
2022-10-23 15:09:44 -0300

    POO(Apostila1) Ex1

---
+++ Carro.java
@@ -0,0 +1,28 @@
+public class Carro {
+    private String modelo;
+    private String cor;
+
+    public void viraEsquerda(String[] args) {
+
+    }
+
+    public void acelera(String[] args) {
+
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String setModelo() {
+        return modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public String setCor() {
+        return cor;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Nome: Bruna Aparecida Barbosa dos Santos 
+RA: 2040482122039
